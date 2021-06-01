f45ec2bdae174969be7288716b93e11db3264407
Eduardo Machado <eduardomachadorezende0@gmail.com>
2021-05-31 22:23:20 -0300

    Fazendo o controller funcionar

--- src/main/java/eduumach/github/com/simuladorbanco/controller/ContaAPI.java
+++ src/main/java/eduumach/github/com/simuladorbanco/controller/ContaAPI.java
@@ -4,14 +4,16 @@ import eduumach.github.com.simuladorbanco.domain.Dados;
 import eduumach.github.com.simuladorbanco.model.Cliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Service
-@Controller
-@RequestMapping("/Conta")
+@RestController
+@RequestMapping("/conta")
 public class ContaAPI {
+
+    @GetMapping("/teste")
+    public String teste(){
+        return "Ola mundão!";
+    }
 
     @PostMapping("/abrir")
     public void abriConta(@RequestBody Dados dados){
