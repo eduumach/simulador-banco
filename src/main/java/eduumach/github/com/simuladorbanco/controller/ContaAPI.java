9cc0ba2bdb281f3d41588a74acb967814dcefb4b
Eduardo Machado <eduardomachadorezende0@gmail.com>
2021-06-05 00:04:10 -0300

    Refatoração

--- src/main/java/eduumach/github/com/simuladorbanco/controller/ContaAPI.java
+++ src/main/java/eduumach/github/com/simuladorbanco/controller/ContaAPI.java
@@ -2,7 +2,6 @@ package eduumach.github.com.simuladorbanco.controller;
 
 import eduumach.github.com.simuladorbanco.domain.Dados;
 import eduumach.github.com.simuladorbanco.model.Cliente;
-import eduumach.github.com.simuladorbanco.model.Conta;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -11,20 +10,21 @@ import java.util.ArrayList;
 @RequestMapping("/conta")
 public class ContaAPI {
     private ArrayList<Cliente> cliente = new ArrayList<Cliente>();
+    private ArrayList<String> cpf = new ArrayList<String>();
     private int id = 0;
-
-    @GetMapping("/teste")
-    public String teste(){
-        return "Ola mundão!";
-    }
 
     @PostMapping("/abrir")
     public String abriConta(@RequestBody Dados dados){
         Cliente c = new Cliente(dados.getNome(), dados.getCpf());
-        cliente.add(c);
-        int idd = id;
-        id ++;
-        return "Seu Id; "+ idd ;
+        if(!cpf.contains(dados.getCpf())){
+            cpf.add(dados.getCpf());
+            cliente.add(c);
+            int idd = id;
+            id ++;
+            return "Seu Id; "+ idd;
+        }else {
+            return "Já cadastrado";
+        }
     }
 
     @PostMapping("/{id}/deposito/{valor}")
