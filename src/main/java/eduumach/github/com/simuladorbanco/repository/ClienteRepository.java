45bd207c5139628ac2b1b6958d693d418767c47c
Eduardo Machado Rezende <eduardomachadorezende0@gmail.com>
2021-09-23 23:27:47 -0300

    Adição: findByCpf

--- src/main/java/eduumach/github/com/simuladorbanco/repository/ClienteRepository.java
+++ src/main/java/eduumach/github/com/simuladorbanco/repository/ClienteRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
+    ClienteEntity findByCpf(String cpf);
 }
